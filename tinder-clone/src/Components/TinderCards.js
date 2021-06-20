220a11eafbcc795cdce8a3739bd91e33ab303086
Oshanik22 <oshanik22@gmail.com>
2021-06-21 01:51:59 +0530

    hopefully final commit

--- tinder-clone/src/Components/TinderCards.js
+++ tinder-clone/src/Components/TinderCards.js
@@ -1,31 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
 import "./TinderCards.css";
 import TinderCard from 'react-tinder-card';
 import { SwipeableDrawer } from '@material-ui/core';
+import axios from './axios';
+
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Daenerys Targaryen",
-            url: "https://c4.wallpaperflare.com/wallpaper/414/301/201/game-of-thrones-5k-4k-8k-wallpaper-preview.jpg"
-        },
-        {
-            name: "Rachel Green",
-            url: "https://s1.r29static.com/bin/entry/aeb/430x516,85/1356319/image.webp"
-        },
-        {
-            name: "Monica Geller",
-            url: "https://i.pinimg.com/564x/27/3e/f2/273ef299637dcff525f246f6f808715d.jpg"
-        },
-        {
-            name: "Pheobe Buffay",
-            url: "https://i.ytimg.com/vi/syxbHuZY6lU/maxresdefault.jpg"
-        },
-        // {
-        //     name: "Ella Lopez",
-        //     url: "https://i.pinimg.com/564x/5a/b6/79/5ab679b4e5a48ade49e58936d3c3b6bb.jpg"
-        // },
-    ]);
+    const [people, setPeople] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+            setPeople(req.data);
+        }
+        fetchData();
+    }, [])
 
     const swiped = (direction, nameToDelete)=>{
         console.log("removing: "+nameToDelete)
@@ -48,7 +37,7 @@ function TinderCards() {
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >    
                         <div
-                            style={{ backgroundImage: `url(${person.url})` }}
+                            style={{ backgroundImage: `url(${person.imgUrl})` }}
                             className="card"
                         >
                             <h3>{person.name}</h3>
@@ -63,3 +52,28 @@ function TinderCards() {
 }
 
 export default TinderCards 
+
+
+/*
+
+json for post request
+[
+        {
+            "name": "Daenerys Targaryen",
+            "imgUrl": "https://c4.wallpaperflare.com/wallpaper/414/301/201/game-of-thrones-5k-4k-8k-wallpaper-preview.jpg"
+        },
+        {
+            "name": "Rachel Green",
+            "imgUrl": "https://s1.r29static.com/bin/entry/aeb/430x516,85/1356319/image.webp"
+        },
+        {
+            "name": "Monica Geller",
+            "imgUrl": "https://i.pinimg.com/564x/27/3e/f2/273ef299637dcff525f246f6f808715d.jpg"
+        },
+        {
+            "name": "Pheobe Buffay",
+            "imgUrl": "https://i.ytimg.com/vi/syxbHuZY6lU/maxresdefault.jpg"
+        }
+]
+
+*/ 
